ea2dd717530f3953a76b221db9055a294f2c82b0
singhdevendrajtz <singhdevendrajtz@gmail.com>
2025-09-16 19:54:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,38 +1,59 @@
 //complete this code
 class Rectangle {
-	constructor(width,height)
-	{
-	this._height=height;
-	this._width=width;
-	}
-	get width()
-	{
-	return(this._width);
-	}
-	get height()
-	{
-	return(this._height);
-	}
-	getArea()
-	{
-		
-		return(this._width * this._height );
-	}
+  /**
+   * Initializes a new Rectangle with a given width and height.
+   * @param {number} width The width of the rectangle.
+   * @param {number} height The height of the rectangle.
+   */
+  constructor(width, height) {
+    this._width = width;
+    this._height = height;
+  }
+
+  /**
+   * Getter method to retrieve the width of the rectangle.
+   * @returns {number} The width of the rectangle.
+   */
+  get width() {
+    return this._width;
+  }
+
+  /**
+   * Getter method to retrieve the height of the rectangle.
+   * @returns {number} The height of the rectangle.
+   */
+  get height() {
+    return this._height;
+  }
+
+  /**
+   * Calculates the area of the rectangle.
+   * @returns {number} The area of the rectangle.
+   */
+  getArea() {
+    return this.width * this.height;
+  }
 }
 
+// 2. Square Class
 class Square extends Rectangle {
-	constructor(side);
-	{
-	this.side=side;
-	super(side,side);
-	}
-getPerimeter()
-{
+  /**
+   * Initializes a new Square with a given side length.
+   * Inherits from the Rectangle class, setting both width and height to the same value.
+   * @param {number} side The side length of the square.
+   */
+  constructor(side) {
+    super(side, side); // Call the parent (Rectangle) constructor
+  }
 
-	return(this.side*4);
+  /**
+   * Calculates the perimeter of the square.
+   * @returns {number} The perimeter of the square.
+   */
+  getPerimeter() {
+    return 4 * this.width;
+  }
 }
-}
-
 // Do not change the code below this line
 window.Rectangle = Rectangle;
 window.Square = Square;
